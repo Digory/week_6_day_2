187c4a8b8b1db1add7338eb7f17bdea804fe45ab
Digory <digory1@googlemail.com>
2018-07-31 19:14:42 +0100

    Add method calls to Runner

--- src/Runner.java
+++ src/Runner.java
@@ -3,11 +3,17 @@ public class Runner {
     public static void main(String[] args){
 
         Library library = new Library(100);
-        Book catPictures = new Book("Cat Pictures 2", BookGenre.NONFICTION);
+        Book catPictures = new Book("Cat Pictures", BookGenre.NONFICTION);
+        Book catPictures2 = new Book("Cat Pictures 2", BookGenre.NONFICTION);
+        Book catPicturesUnseen = new Book("Cat Pictures - NEVER SEEN BEFORE", BookGenre.OTHER);
         Borrower digory = new Borrower();
 
         library.addBook(catPictures);
+        library.addBook(catPictures2);
+        library.addBook(catPicturesUnseen);
 
+        System.out.println(digory.borrowBookFromLibrary(library));
+        System.out.println(digory.borrowBookFromLibrary(library));
         System.out.println(digory.borrowBookFromLibrary(library));
         System.out.println(digory.borrowBookFromLibrary(library));
 
